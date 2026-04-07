24ed06765da69f38211f28a09166458a392104ed
jasnarahman <jasnaabdua@gmail.com>
2026-04-07 15:48:00 +0530

    completed

---
+++ StackDemo.java
@@ -0,0 +1,61 @@
+class GenericStack<T> {
+    private T[] stackArray;
+    private int top;
+    private int capacity;
+
+    @SuppressWarnings("unchecked")
+    public GenericStack(int capacity) {   // ✔ fixed constructor
+        this.capacity = capacity;
+        this.top = -1;
+        this.stackArray = (T[]) new Object[capacity];
+    }
+
+    // push operation
+    public void push(T element) {
+        // Stack Overflow Check
+        if (top == capacity - 1) {
+            System.out.println("Error: stack Overflow. Cannot push " + element);
+            return;
+        }
+        stackArray[++top] = element;
+        System.out.println("Pushed: " + element);
+    }
+
+    // pop operation
+    public T pop() {
+        // stack underflow check
+        if (top == -1) {
+            System.out.println("Error: stack Underflow. The stack is empty");
+            return null;
+        }
+        T poppedElement = stackArray[top--];
+        return poppedElement;
+    }
+}
+
+public class StackDemo {
+    public static void main(String[] args) {
+
+        System.out.println("-----Integer Stack Operations--------");
+        GenericStack<Integer> intStack = new GenericStack<>(3);
+
+        intStack.push(10);
+        intStack.push(20);
+        intStack.push(30);
+        intStack.push(40); // overflow
+
+        System.out.println("Popped: " + intStack.pop());
+
+        System.out.println("\n-----String Stack Operations----");
+
+        GenericStack<String> stringStack = new GenericStack<>(2);
+
+        stringStack.push("KTU");
+        stringStack.push("MCA");
+
+        System.out.println("Popped: " + stringStack.pop());
+        System.out.println("Popped: " + stringStack.pop());
+
+        stringStack.pop(); // underflow
+    }
+}
